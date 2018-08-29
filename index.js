20438815974e5659e8e0dfa554eb7744a355acdd
Gina Fedder <ginafedder@yahoo.com>
2018-08-29 16:04:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var emptyArray = [musicians[i] + " plays " + instruments[i]];
   var i;
+  var emptyArray = [musicians[i] + " plays " + instruments[i]];
   for (i = 0; i < musicians.length; i++) {
     return emptyArray;
   }
